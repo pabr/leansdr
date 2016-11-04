bb3599bdb76f271b2aa1f3a3b71d738b3a72f217
pabr <pabr@pabr.org>
2016-11-04 10:40:15 +0100

    Add complex ops

--- src/leansdr/math.h
+++ src/leansdr/math.h
@@ -19,8 +19,18 @@ namespace leansdr {
   }
 
   template<typename T>
+  complex<T> operator *(const complex<T> &a, const complex<T> &b) {
+    return complex<T>(a.re*b.re-a.im*b.im, a.re*b.im+a.im*b.re);
+  }
+
+  template<typename T>
   complex<T> operator *(const complex<T> &a, const T &k) {
     return complex<T>(a.re*k, a.im*k);
+  }
+
+  template<typename T>
+  complex<T> operator *(const T &k, complex<T> &a) {
+    return complex<T>(k*a.re, k*a.im);
   }
 
 }  // namespace
