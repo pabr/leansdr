d65ca0fde0e6243a3409ef7a368dda7779f3f86c
pabr <pabr@pabr.org>
2017-02-27 12:23:55 +0100

    Implement RRC filter

--- src/leansdr/filtergen.h
+++ src/leansdr/filtergen.h
@@ -23,6 +23,7 @@ namespace leansdr {
     }
 
     // Generate coefficients for a sinc filter.
+    // https://en.wikipedia.org/wiki/Sinc_filter
 
     template<typename T>
     int lowpass(int order, float Fcut, T **coeffs, float gain=1) {
@@ -42,13 +43,45 @@ namespace leansdr {
       normalize_dcgain(ncoeffs, *coeffs, gain);
       return ncoeffs;
     }
+
     
+    // Generate coefficients for a RRC filter.
+    // https://en.wikipedia.org/wiki/Root-raised-cosine_filter
+
     template<typename T>
-    int root_raised_cosine (float Fm, float Fs, float rolloff, T **coeffs) {
-      int ncoeffs = 0;
+    int root_raised_cosine(int order, float Fs, float rolloff, T **coeffs) {
+      float B = rolloff, pi = M_PI;
+      int ncoeffs = (order+1) | 1;
       *coeffs = new T[ncoeffs];
-      fail("not implemented");
+      for ( int i=0; i<ncoeffs; ++i ) {
+	int t = i - ncoeffs/2;
+	float c;
+	if ( t == 0 )
+	  c = sqrt(Fs) * (1-B+4*B/pi);
+	else {
+	  float tT = t * Fs;
+	  float den = pi*tT*(1-(4*B*tT)*(4*B*tT));
+	  if ( ! den )
+	    c = B*sqrt(Fs/2) * ( (1+2/pi)*sin(pi/(4*B)) +
+				 (1-2/pi)*cos(pi/(4*B)) );
+	  else
+	    c = sqrt(Fs) * ( sin(pi*tT*(1-B)) +
+			     4*B*tT*cos(pi*tT*(1+B)) ) / den;
+	}
+	(*coeffs)[i] = c;
+      }
+      normalize_dcgain(ncoeffs, *coeffs);
       return ncoeffs;
+    }
+
+
+    // Dump filter coefficients for matlab/octave
+
+    void dump_filter(const char *name, int ncoeffs, float *coeffs) {
+      fprintf(stderr, "%s = [", name);
+      for ( int i=0; i<ncoeffs; ++i )
+	fprintf(stderr, "%s %f", (i?",":""), coeffs[i]);
+      fprintf(stderr, " ];\n");
     }
 
   }  // namespace
