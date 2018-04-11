e6416aa323fdfa553b7cb1420c3291acaeb7b65a
pabr <pabr@pabr.org>
2018-04-11 11:50:34 +0200

    Clarify license.

--- src/apps/leandvbtx.cc
+++ src/apps/leandvbtx.cc
@@ -1,5 +1,19 @@
-// leandvbtx.cc copyright (c) 2016-2017 pabr@pabr.org
-// http://www.pabr.org/radio/leandvb
+// This file is part of LeanSDR Copyright (C) 2016-2018 <pabr@pabr.org>.
+// See the toplevel README for more information.
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 
 #include <stdio.h>
 #include <stdlib.h>
